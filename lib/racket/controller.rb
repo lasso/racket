6980409ee61e038a9c3e99c2f69202b48d3883a1
Lars Olsson <lasso@lassoweb.se>
2015-05-14 16:19:03 +0200

    Add documentation

--- lib/racket/controller.rb
+++ lib/racket/controller.rb
@@ -19,8 +19,11 @@ along with Racket.  If not, see <http://www.gnu.org/licenses/>.
 =end
 
 module Racket
+
+  # Base controller class. Your controllers should inherit this class.
   class Controller
 
+    # :nodoc:
     def self.inherited(klass)
       Application.options[:last_added_controller].push(klass)
     end
@@ -53,19 +56,38 @@ module Racket
       return @options.fetch(option_name.to_sym, nil)
     end
 
+    # Sets an option for the current controller class.
+    #
+    # @param [Symbol] option_name
+    # @param [Object] option_value
     def self.set_option(option_name, option_value)
       @options ||= {}
       @options[option_name.to_sym] = option_value
     end
 
+    # Returns a route to an action within the current controller.
+    #
+    # @param [Symbol] action
+    # @param [Array] params
+    # @return [String]
     def rs(action, *params)
       Application.get_route(self.class, action, params)
     end
 
+    # Returns a route to an action within another controller.
+    #
+    # @param [Class] controller
+    # @param [Symbol] action
+    # @param [Array] params
+    # @return [String]
     def r(controller, action, *params)
       Application.get_route(controller, action, params)
     end
 
+    # Renders an action.
+    #
+    # @param [Symbol] action
+    # @return [String]
     def render(action)
       __execute(action)
       Application.view_cache.render(self)
